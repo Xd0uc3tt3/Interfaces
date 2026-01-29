c01d545115a7b5de9b19eae2afdde446d6dfd300
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-01-29 17:38:23 -0400

    Added enemy move function
    The functionality is temporary, just wanted to make sure i could get it to work

--- Program.cs
+++ Program.cs
@@ -29,24 +29,91 @@ namespace Interfaces
         }
     }
 
+    public interface IMoveStrategy
+    {
+        Position Move(Position currentPosition);
+    }
+
+    public class AggressiveMoveStrategy : IMoveStrategy
+    {
+        public Position Move(Position currentPosition)
+        {
+            return new Position(currentPosition.x + 1, currentPosition.y);
+        }
+    }
+
+    public class PassiveMoveStrategy : IMoveStrategy
+    {
+        public Position Move(Position currentPosition)
+        {
+            return new Position(currentPosition.x - 1, currentPosition.y);
+        }
+    }
+
     public class Enemy
     {
         public Position Position;
         public ConsoleColor Color;
+        public IMoveStrategy _moveStrategy;
 
-        public Enemy(Position position, ConsoleColor color)
+        public Enemy(Position position, ConsoleColor color, IMoveStrategy moveStrategy)
         {
             Position = position;
             Color = color;
+            _moveStrategy = moveStrategy;
         }
 
+        public void Move()
+        {
+            Position = _moveStrategy.Move(Position);
+        }
     }
 
 
     internal class Program
     {
+        public static Player player;
         static void Main(string[] args)
         {
+            player = new Player(new Position(10, 10), ConsoleColor.Green);
+
+            IMoveStrategy aggressive = new AggressiveMoveStrategy();
+            IMoveStrategy passive = new PassiveMoveStrategy();
+
+            Enemy enemy = new Enemy(new Position(5, 5), ConsoleColor.Red, aggressive);
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("(M) Move Enemy");
+                Console.WriteLine("(I) Aggressive");
+                Console.WriteLine("(O) Passive");
+                Console.WriteLine("(P) Random");
+                Console.WriteLine(enemy._moveStrategy.GetType().Name);
+                Console.WriteLine();
+
+                Console.ForegroundColor = player.Color;
+                Console.SetCursorPosition(player.Position.x, player.Position.y);
+                Console.Write("P");
+
+                Console.ForegroundColor = enemy.Color;
+                Console.SetCursorPosition(enemy.Position.x, enemy.Position.y);
+                Console.Write("E");
+
+                Console.ResetColor();
+
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.M)
+                    enemy.Move();
+                else if (key == ConsoleKey.I)
+                    enemy._moveStrategy = aggressive;
+                else if (key == ConsoleKey.O)
+                    enemy._moveStrategy = passive;
+            }
         }
     }
 }
