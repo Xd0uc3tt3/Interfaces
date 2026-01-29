b315cfd314c17773ded09c1f276d9e8f491ee40c
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-01-29 17:11:18 -0400

    Added player class and position struct

--- Program.cs
+++ Program.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace Interfaces
 {
+    public struct Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class Player
+    {
+        public Position Position;
+        public ConsoleColor Color;
+
+        public Player(Position position, ConsoleColor color)
+        {
+            Position = position;
+            Color = color;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
